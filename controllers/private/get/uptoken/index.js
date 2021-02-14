b508edfdd0ce92addb3b936530b5b9967aef6693
wodewone <wodewone@sina.com>
2021-02-14 23:54:08 +0800

    fix upload file

--- controllers/private/get/uptoken/index.js
+++ controllers/private/get/uptoken/index.js
@@ -1,5 +1,6 @@
 const { qiniu: { access, secret } } = require('config');
 const qiniu = require('qiniu');
+const md5 = require('md5');
 const { qiniu: { cdnUrl: domain } } = require('config');
 
 module.exports = (ctx, next) => {
@@ -8,18 +9,23 @@ module.exports = (ctx, next) => {
         ctx.result = jwtData;
         return next();
     }
-    const { data: scope } = jwtData;
+    const { data: userId } = jwtData;
     const mac = new qiniu.auth.digest.Mac(access, secret);
+    const scope = md5(userId);
 
     const options = {
-        scope,
-        returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
+        scope: `wodewone:${scope}`,
+        isPrefixalScope: 1,
+        returnBody: '{"key":"$(key)","width":"$(imageInfo.width)","height":"$(imageInfo.height)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
     };
     const putPolicy = new qiniu.rs.PutPolicy(options);
     const token = putPolicy.uploadToken(mac);
+    // const url = `${domain}/${scope}`;
+    const url = domain;
     ctx.result = {
         token,
-        domain,
+        url,
+        scope,
     };
     return next();
 };
